f7aa6e3ea44b02763e4a23caa70dd9178e820a7f
dilan <dilan@Dilan>
2022-10-16 19:16:11 -0500

    v1

---
+++ src/logica/Proyecto_capas.java
@@ -0,0 +1,22 @@
+
+package logica;
+
+import gui.Pantalla;
+
+/**
+ *
+ * @author dilan
+ */
+public class Proyecto_capas {
+
+    
+    public static void main(String[] args) {
+      
+        Pantalla panta  = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
